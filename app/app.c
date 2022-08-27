2e790b18ebbdbe12491c90c965ff3933cec187b9
MiguelHIteso <miguel.hernandez@iteso.mx>
2022-08-27 14:14:03 -0500

    bubble sort funcional

--- app/app.c
+++ app/app.c
@@ -1,4 +1,19 @@
+#include "pointers.h"
+void print_collection(int a[], int n)
+{
+    for(int i = 0; i < 13; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
 int main(void)
 {
-  return 0;
+    int a[]= {3,4,5,2,1,8,9,10,7,2,11,45,6,9};
+    print_collection(a,13);
+    bubble_sort(&a,13);
+    print_collection(a,13);
+
+
+    return 0;
 }
--- lib/pointers.c
+++ lib/pointers.c
@@ -4,10 +4,25 @@
 //
 
 #include "pointers.h"
-
+void swap(int *a, int *b)
+{
+    int n = *a;
+    *a = *b;
+    *b = n;
+}
 int bubble_sort(int *target, int count)
 {
   // aqui su implementación.
+    for(int i = 0; i < count - 2; i++)
+    {
+        for(int j = 0; j < count - 1; j++)
+        {
+            if(*(target + j) > *(target + j + 1))
+            {
+                swap((target + j),(target + j + 1));
+            }
+        }
+    }
   return 0;
 }
 
