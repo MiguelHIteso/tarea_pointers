e16586a63daaa4ab63d887c431074bf3561b5fbb
MiguelHIteso <miguel.hernandez@iteso.mx>
2022-08-27 20:21:25 -0500

    pruebas eliminadas

--- app/app.c
+++ app/app.c
@@ -1,26 +1,5 @@
 #include "pointers.h"
-void print_collection(int a[], int n)
-{
-    for(int i = 0; i < 13; i++)
-    {
-        printf("%d ",a[i]);
-    }
-    printf("\n");
-}
 int main(void)
 {
-    int a[]= {3,4,5,2,1,8,9,10,7,2,11,45,6,9};
-    print_collection(a,13);
-    bubble_sort(&a,13);
-    print_collection(a,13);
-    int b[]= {3,4,5,2,1,8,9,10,7,2,11,45,6,9};
-    print_collection(b,13);
-    insertion_sort(&b,13);
-    print_collection(b,13);
-
-    reverse(&b,13);
-
-    print_collection(b,13);
-    calulate_frequency(b,13,2);
     return 0;
 }
